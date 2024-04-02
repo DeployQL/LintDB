a6e5b87c7f633772a31a6b33fdc5fbe95d1d60e8
Matt Barta <mtbarta@gmail.com>
2024-04-02 15:12:38 +0000

    remove blas_include_dirs from python script

--- conda/lintdb/build-pkg.sh
+++ conda/lintdb/build-pkg.sh
@@ -13,4 +13,4 @@ make -C _build_python_${PY_VER} -j$(nproc) pylintdb
 
 # Build actual python module.
 cd _build_python_${PY_VER}/lintdb/python
-$PYTHON setup.py install --blas_include_dirs=${CONDA_PREFIX}/include --blas=openblas --single-version-externally-managed --record=record.txt --prefix=$PREFIX
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt --prefix=$PREFIX
