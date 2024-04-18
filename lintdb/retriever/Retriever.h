fba0d05051a2af9da3f83bd2a071e81e394ba1d2
Matt Barta <mtbarta@gmail.com>
2024-04-18 17:55:00 +0000

    add march=native

--- lintdb/retriever/Retriever.h
+++ lintdb/retriever/Retriever.h
@@ -15,6 +15,7 @@ namespace lintdb {
         const size_t k_top_centroids;
         const size_t n_probe;
         const size_t num_docs_to_score = 4000;
+        const size_t out_second_stage = 1000;
     };
     
     struct Retriever {
