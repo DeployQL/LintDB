1685094cacbb1e405f3db79570851f697e29689b
Matt Barta <mtbarta@gmail.com>
2024-06-15 17:41:33 +0000

    remove mkl root from linux. upgrade mkl

--- conda/lintdb/build-lib.sh
+++ conda/lintdb/build-lib.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-MKLROOT=_build/vcpkg_installed/x64-linux/lib/intel64 cmake -B _build \
+cmake -B _build \
       -DBUILD_SHARED_LIBS=ON \
       -DBUILD_TESTING=OFF \
       -DENABLE_PYTHON=OFF \
@@ -10,7 +10,7 @@ MKLROOT=_build/vcpkg_installed/x64-linux/lib/intel64 cmake -B _build \
       -DBLA_VENDOR=Intel10_64lp \
       -DCMAKE_BUILD_TYPE=Release .
 
-MKLROOT=_build/vcpkg_installed/x64-linux/lib/intel64 make -C _build -j$(nproc) lintdb
+make -C _build -j$(nproc) lintdb
 
 cmake --install _build --prefix $PREFIX
 cmake --install _build --prefix _liblintdb_stage/
