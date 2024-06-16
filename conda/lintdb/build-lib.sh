aba3576fddb31bac6e94f504049252fe2b073913
Matt Barta <mtbarta@gmail.com>
2024-06-16 20:01:16 +0000

    use clang on linux. link against iomp for liblintdb

--- conda/lintdb/build-lib.sh
+++ conda/lintdb/build-lib.sh
@@ -8,7 +8,11 @@ MKLROOT=_build/vcpkg_installed/x64-linux/lib/intel64 cmake -B _build \
       -DENABLE_PYTHON=OFF \
       -DCMAKE_INSTALL_LIBDIR=lib \
       -DBLA_VENDOR=Intel10_64lp \
-      -DCMAKE_BUILD_TYPE=Release .
+      -DCMAKE_BUILD_TYPE=Release \
+      -DOpenMP_CXX_FLAGS=-fopenmp=libiomp5 \
+      -DOpenMP_CXX_LIB_NAMES=libiomp5 \
+      -DOpenMP_libiomp5_LIBRARY=${ROOT_DIR}/_build_python_/vcpkg_installed/x64-linux/lib/intel64/libiomp5.so \
+      .
 
 MKLROOT=_build/vcpkg_installed/x64-linux/lib/intel64 make -C _build -j$(nproc) lintdb
 
