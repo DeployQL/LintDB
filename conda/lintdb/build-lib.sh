1bad63701c601b8c61ef95a8bb91c20f39450cf7
Matt <mtbarta@gmail.com>
2024-06-16 20:48:14 -0500

    Bump version: 0.3.1 → 0.4.0 (#29)
    Signed-off-by: Matt <mtbarta@gmail.com>

--- conda/lintdb/build-lib.sh
+++ conda/lintdb/build-lib.sh
@@ -2,15 +2,19 @@
 
 set -e
 
-cmake -B _build \
+MKLROOT=_build/vcpkg_installed/x64-linux/lib/intel64 cmake -B _build \
       -DBUILD_SHARED_LIBS=ON \
       -DBUILD_TESTING=OFF \
       -DENABLE_PYTHON=OFF \
       -DCMAKE_INSTALL_LIBDIR=lib \
       -DBLA_VENDOR=Intel10_64lp \
-      -DCMAKE_BUILD_TYPE=Release .
+      -DCMAKE_BUILD_TYPE=Release \
+      -DOpenMP_CXX_FLAGS=-fopenmp=libiomp5 \
+      -DOpenMP_CXX_LIB_NAMES=libiomp5 \
+      -DOpenMP_libiomp5_LIBRARY=$PREFIX/lib/libiomp5.so \
+      .
 
-make -C _build -j$(nproc) lintdb
+MKLROOT=_build/vcpkg_installed/x64-linux/lib/intel64 make -C _build -j$(nproc) lintdb
 
 cmake --install _build --prefix $PREFIX
 cmake --install _build --prefix _liblintdb_stage/
